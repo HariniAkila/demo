ea992449f36b781d12d70bf82a15729a8ad12d14
Manikandan <manikandanm441@gmail.com>
2024-02-06 11:59:40 +0530

    First commit

---
+++ samplecalc/src/test/java/samplecalc/AdditionTest.java
@@ -0,0 +1,13 @@
+package samplecalc;
+
+import org.testng.annotations.Test;
+import org.testng.Assert;
+
+public class AdditionTest {
+	@Test
+	public void add()
+	{
+		 Assert.assertEquals(2, 2);
+	}
+
+}
